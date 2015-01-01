dffdd686d204a9318086f6fb1ddfb6bec1708208
showi <joachim.basmaison@gmail.com>
2015-01-01 02:23:10 +0100

    Fix Type: --pw-min replace by --pw-max

--- src/engines/hashcat.cpp
+++ src/engines/hashcat.cpp
@@ -464,7 +464,7 @@ int Ngn_oclHashcat::run ()
 	// if mask maximum set, apply it
 	if(job->getMaskMax())
 	{
-		cmd = format("%s --pw-min %d",
+		cmd = format("%s --pw-max %d",
 			cmd,
 			job->getMaskMax()
 		);
